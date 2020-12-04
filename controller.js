7fe4d617b1d9817578e9b4824f45cc5dafc5cd4b
Septian Pratama <septianpratama18@gmail.com>
2020-12-04 10:37:09 +0700

    berhasil ubah & hapus data

--- controller.js
+++ controller.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-var response = require('./res');
-var connection = require('./koneksi');
-const { connect } = require('./koneksi');
+var response = require("./res");
+var connection = require("./koneksi");
+const { connect } = require("./koneksi");
 
 exports.index = function(req, res) {
-    response.ok("Aplikasi REST API ku berjalan!", res)
+    response.ok("Aplikasi REST API ku berjalan!", res);
 };
 
 // Menampilkan semua data mahasiswa
 exports.tampilSemuaMahasiswa = function(req, res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, fields) {
         if (error) {
             connection.log(error);
         } else {
-            response.ok(rows, res)
+            response.ok(rows, res);
         }
     });
 };
@@ -22,16 +22,17 @@ exports.tampilSemuaMahasiswa = function(req, res) {
 // Menampilkan semua data mahasiswa berdasarkan id
 exports.tampilMahasiswaByID = function(req, res) {
     let id = req.params.id;
-    connection.query('SELECT * FROM mahasiswa WHERE id_mhs = ?', [id],
+    connection.query(
+        "SELECT * FROM mahasiswa WHERE id_mhs = ?", [id],
         function(error, rows, fields) {
             if (error) {
                 console.log(error);
             } else {
                 response.ok(rows, res);
             }
-        });
+        }
+    );
 };
-
 
 // Menambahkan data mahasiswa
 exports.tambahMahasiswa = function(req, res) {
@@ -39,12 +40,47 @@ exports.tambahMahasiswa = function(req, res) {
     var nama = req.body.nama;
     var jurusan = req.body.jurusan;
 
-    connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)', [nim, nama, jurusan],
+    connection.query(
+        "INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)", [nim, nama, jurusan],
         function(error, rows, fields) {
             if (error) {
                 console.log(error);
             } else {
-                response.ok("Berhasil Menambahkan Data!", res)
+                response.ok("Berhasil Menambahkan Data!", res);
             }
-        });
+        }
+    );
+};
+
+// Mengubah data berdasarkan ID
+exports.ubahMahasiswa = function(req, res) {
+    var id = req.body.id_mhs;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query(
+        "UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mhs=?", [nim, nama, jurusan, id],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Ubah Data", res);
+            }
+        }
+    );
+};
+
+// Menghapus data berdasarkan ID
+exports.hapusMahasiswa = function(req, res) {
+    var id = req.body.id_mhs;
+    connection.query('DELETE FROM mahasiswa WHERE id_mhs=?', [id],
+        function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Hapus Data", res);
+            }
+        }
+    );
 };
--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 module.exports = function(app) {
-    var jsonku = require('./controller');
+    var jsonku = require("./controller");
 
-    app.route('/')
-        .get(jsonku.index);
+    app.route("/").get(jsonku.index);
 
-    app.route('/tampil')
-        .get(jsonku.tampilSemuaMahasiswa);
+    app.route("/tampil").get(jsonku.tampilSemuaMahasiswa);
 
-    app.route('/tampil/:id')
-        .get(jsonku.tampilMahasiswaByID);
+    app.route("/tampil/:id").get(jsonku.tampilMahasiswaByID);
 
-    app.route('/tambah')
-        .post(jsonku.tambahMahasiswa);
-}
+    app.route("/tambah").post(jsonku.tambahMahasiswa);
+
+    app.route("/ubah").put(jsonku.ubahMahasiswa);
+
+    app.route("/hapus").delete(jsonku.hapusMahasiswa);
+};
